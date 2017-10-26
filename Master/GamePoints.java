417512d52a1caf7cd9289ff77b5b93c45621fd4a
HARI KRISHNA PARIVEDA <hknitw@gmail.com>
2017-10-25 20:48:20 -0700

    Initial Commit

---
+++ Master/GamePoints.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo) // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+//import java.awt.Color;
+/**
+ * Write a description of class GamePoints here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GamePoints extends Actor implements scoreObserver
+{
+    /**
+     * Act - do whatever the GamePoints wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+   public static int score=0;
+   public void update(int points)
+   {
+    
+    score=points;
+    act();
+   }
+    
+   public int getscore()
+   {
+       return score;
+    }
+   
+   public void act() 
+    {
+        //System.out.println(getscore());
+        // Add your action code here.
+        setImage(new GreenfootImage("Score : "+ getscore() , 24 , Color.BLACK, Color.WHITE));
+    }    
+}
---
+++ Master/blueDiamond.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bluepoints here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class blueDiamond extends Actor
+{
+    /**
+     * Act - do whatever the bluepoints wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public static int score=0;
+   public void update(int points)
+   {
+    
+    score=points;
+    act();
+   }
+   
+   public int getscore()
+   {
+       return score;
+    }
+    public void act() 
+    {
+        // Add your action code here.
+     //   System.out.println(bluep);
+        setImage(new GreenfootImage("Score : "+ getscore() , 24 , Color.BLACK, Color.WHITE));
+    }    
+}
