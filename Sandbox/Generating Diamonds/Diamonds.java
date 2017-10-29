99e72c1159f46ada9d23a6551be89a5b3f7d01f5
Radhika Srinivasan <31632389+radhikasrinivasan8@users.noreply.github.com>
2017-10-28 21:00:24 -0700

    Create Diamonds.java

---
+++ Sandbox/Generating Diamonds/Diamonds.java
@@ -0,0 +1,12 @@
+import greenfoot.*; 
+/**
+ * Write a description of class Diamonds here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Diamonds  
+{
+    // instance variables - replace the example below with your own
+    
+}
