4b49d5f0f91085a48f7304616c96d05292f10480
Radhika Srinivasan <radhika.srinivasan@sjsu.edu>
2017-11-12 22:42:23 -0800

    made Diamond code changes

--- Master/Diamonds.java
+++ Master/Diamonds.java
@@ -2,37 +2,54 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Diamonds here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public abstract class Diamonds extends Actor
+public  class Diamonds extends Actor
 {
-   
-    
-    
+
+
+
 private int velocity = 3;
     public int x;
     private int outOfBoundary = 0;
-    
-    
-   
+
+
+
     public void moveLeft ()
         {
             setLocation(getX() - velocity, getY());
         }
-  
-        
-    public void act() 
+
+      public void despawning()
+    {
+        x=getX();
+
+        if (x==0)
+        {
+            getWorld().removeObject(this);
+        }
+        else if(isTouching(Brave.class))
+        {
+            getWorld().removeObject(this);
+            Greenfoot.playSound("bell_3.wav");
+
+        }
+    }
+
+
+    public void act()
     {
     moveLeft();
-    
+
     if (getX() <= outOfBoundary)
          {
              getWorld().removeObject(this);
         }
-        
-    
-    } 
+      despawning();
+
+    }
+
+
 }
-    
